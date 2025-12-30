cd117ec6122fb9d1e273d0b8358a555e13934837
Daniel18-dan <bernarddaniel171@gmail.com>
2025-12-30 01:37:38 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,15 @@ variable "i_type" {
   type = string
 }
 
+variable "environment" {
+  description = "Environment name (DEV / UAT / PROD)"
+  type        = string
+}
+
+variable "owner" {
+  description = "Resource owner"
+  type        = string
+  default     = "Devops/cloud"
+}
+
+
