5dcbea383d8d6103607e6585e835d2815085e7b4
Daniel18-dan <bernarddaniel171@gmail.com>
2025-12-30 22:50:54 -0800

    Update variables.tf

--- task-03/variables.tf
+++ task-03/variables.tf
@@ -1,8 +1,21 @@
+
 variable "ami_id" {
-  type = string
+  description = "AMI ID for EC2"
+  type        = string
 }
 
-variable "instance_type" {
-  type = string
+variable "i_type" {
+  description = "EC2 instance type"
+  type        = string
 }
 
+variable "environment" {
+  description = "Deployment environment (DEV / UAT / PROD)"
+  type        = string
+}
+
+variable "owner" {
+  description = "Resource owner"
+  type        = string
+  default     = "Daniel-DevOps"
+}
